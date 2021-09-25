381a052ad8e4d63c7e26fe1255fbe1d70c59e2ce
ethiry99 <eric.thiry99@gmail.com>
2021-09-25 11:06:17 -0500

    included loading library in code

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,3 +1,6 @@
+#load dplyr library
+library(dplyr)
+
 # Read in provided MechaCar_mpg.csv file
 Mecha_mpg <- read.csv(file="Resources/MechaCar_mpg.csv")
 
@@ -19,3 +22,26 @@ print (total_summary)
 Lot_coil <-group_by(Mecha_coil,Manufacturing_Lot)
 lot_summary<-Lot_coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
 print(lot_summary)
+
+# t.test all manufacturing lots in total to determine if psi is statistically different from population mean of 1500 psi
+t.test(Mecha_coil$PSI,mu=1500,paired = FALSE)
+
+#Create sample of lot1 
+lot_1 <- subset(Mecha_coil, Manufacturing_Lot == "Lot1")
+
+# t.test lot_1 sample to determine if psi is statistically different from population mean of 1500 psi
+t.test(lot_1$PSI,mu=1500,paired = FALSE)
+
+#Create sample of lot2 
+lot_2 <- subset(Mecha_coil, Manufacturing_Lot == "Lot2")
+
+# t.test lot_2 sample to determine if psi is statistically different from population mean of 1500 psi
+t.test(lot_2$PSI,mu=1500,paired = FALSE)
+
+#Create sample of lot3 
+lot_3 <- subset(Mecha_coil, Manufacturing_Lot == "Lot3")
+
+# t.test lot_3 sample to determine if psi is statistically different from population mean of 1500 psi
+t.test(lot_3$PSI,mu=1500,paired = FALSE)
+
+
